fd99938f6a5c1ad456c382c9ffd89a3d575c339a
Wojciech Kubiak <282127@uwr.edu.pl>
2018-11-17 02:12:22 +0100

    Create hello-world.js

---
+++ 10 Days of Javascript/hello-world.js
@@ -0,0 +1,46 @@
+<!--
+Print the following two lines of output:
+
+    On the first line, print Hello, World! (this is provided for you in the editor).
+    On the second line, print the contents of parameterVariable.
+-->
+
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+    console.log(parameterVariable);
+}
+
+
+function main() {
+    const parameterVariable = readLine();
+    
+    greeting(parameterVariable);
+}
